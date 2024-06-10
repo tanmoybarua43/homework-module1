a596d8879e5b332934a7638c055973f4a77da69d
Tanmoy-Barua <tanmoybarua43@gmail.com>
2024-06-09 17:56:13 -0700

    updated

--- js/script.js
+++ js/script.js
@@ -5,8 +5,25 @@
 $(document).ready(function() {
   
   $('tbody td').not(':contains("Not Available")').click(function() {
-      
+      var content = $(this).text(); //get text content of cell
+      var selectedCol = $('thead tr th').eq($(this).index()).text();
       $(this).toggleClass('selected');
+      $('#displaySelected').show();
+
+      if ($(this).hasClass("selected")) { //check if selected cell has class 
+        // $('#displaySelected').css("visibility", "visible"); //make display box visible 
+        $('#displaySelected').css("margin","2em 0"); 
+        $('#displaySelected').css("border","1px solid black");
+        $('#displaySelected').css("padding","20px");////add spaces above display box 
+        $('#result').append("<p>"+content+" at "+selectedCol+"</p>"); //add child element with contents of cell
+      } else { //if selected cell don't have class
+      $('#result p:contains('+content+')').remove(); //remove child element
+      if ($('#result').has('p').length == false) { //check if there are any child elements within parent
+      $('#displaySelected').css("visibility","hidden"); //make display box hidden 
+      $('#displaySelected').css("margin-top", "0"); //remove spaces above display box
+      }
+    }
+
   });
 
   
